aae4470ebc556a79f42c4c5b462e2e503a4659f8
Rajesh Kumar <60379726+Rajeshkumar919@users.noreply.github.com>
2022-04-25 02:07:10 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,31 @@
+
+import {
+  Admin,
+  Resource,
+
+
+
+} from 'react-admin';
+import jsonServerProvider from 're-data-json-server';
+import {
+  UserList,
+  UserEdit,
+  UserCreate,
+}from './Users';
+
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+function App(){
+ return (
+  <Admin dataProvider={dataProvider}>
+  <Resource
+  name="users"
+  list={UserList}
+  edit={UserEdit}
+  create={UserCreate}
+  />
+  </Admin>
+ );
+}
+ export default App;
